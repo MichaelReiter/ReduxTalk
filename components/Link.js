f45556dfa3668ec6f4978a90c6fd814000b11406
MichaelReiter <michaelreiter@live.ca>
2016-07-27 23:48:14 -0400

    Update Link code style

--- components/Link.js
+++ components/Link.js
@@ -2,7 +2,11 @@ import React, {PropTypes} from 'react';
 
 const Link = ({active, children, onClick}) => {
   if (active) {
-    return <span>{children}</span>;
+    return (
+      <span>
+        {children}
+      </span>
+    );
   }
 
   return (
